e6524ec596f6266d8864f9e2ff70ca06ceaa530c
1C0D <63018905+1C0D@users.noreply.github.com>
2022-10-07 04:36:24 +0200

    Main

--- main.ts
+++ main.ts
@@ -1,35 +1,73 @@
-import { MarkdownView, Plugin } from "obsidian";
+import { Plugin } from "obsidian";
 
 export default class WindowCollapse extends Plugin {
 	async onload() {
 		this.addCommand({
 			id: "duplicate-line",
 			name: "Duplicate Line",
-			callback: () => this.duplicateLine(),
+			editorCallback: (editor) => this.duplicateLine(editor),
+		});
+		this.addCommand({
+			id: "arrow-down",
+			name: "Arrow Down",
+			hotkeys: [{ modifiers: ["Mod", "Alt"], key: "ArrowDown" }],
+			editorCallback: (editor) => {
+				this.arrows(editor, "↓");
+			},
+		});
+		this.addCommand({
+			id: "arrow-up",
+			name: "Arrow Up",
+			hotkeys: [{ modifiers: ["Mod", "Alt"], key: "ArrowUp" }],
+			editorCallback: (editor) => {
+				this.arrows(editor, "↑");
+			},
+		});
+		this.addCommand({
+			id: "arrow-left",
+			name: "Arrow Left",
+			hotkeys: [{ modifiers: ["Mod", "Alt"], key: "ArrowLeft" }],
+			editorCallback: (editor) => {
+				this.arrows(editor, "←");
+			},
+		});
+		this.addCommand({
+			id: "arrow-right",
+			name: "Arrow Right",
+			hotkeys: [{ modifiers: ["Mod", "Alt"], key: "ArrowRight" }],
+			editorCallback: (editor) => {
+				this.arrows(editor, "→");
+			},
 		});
 	}
 
-	duplicateLine() {
-		const markdownView =
-			this.app.workspace.getActiveViewOfType(MarkdownView);
-
-		if (!markdownView) return;
-
-		const editor = markdownView.editor;
-		let { line: lineNumber, ch: column } = editor.getCursor();
-
+	arrows(editor, arrow) {
+		const { line: lineNumber, ch: column } = editor.getCursor();
 		const lineContent = editor.getLine(lineNumber);
+		const content0 = lineContent.slice(0, column);
+		const content1 = lineContent.slice(column, lineContent.length);
+		const newLineContent = content0 + arrow + content1;
+		editor.setLine(lineNumber, newLineContent);
+		editor.setCursor(lineNumber + 1, column + 1);
+	}
 
-		// if (!lineContent) { //not duplicate empty line?, more intuitive to let do it...
-		// 	return;
-		// }
+	duplicateLine(editor) {
+		// const markdownView =
+		// 	this.app.workspace.getActiveViewOfType(MarkdownView);
 
+		// if (!markdownView) return;
+
+		// const editor = markdownView.editor;
+		const { line: lineNumber, ch: column } = editor.getCursor();
+		const lineContent = editor.getLine(lineNumber);
+		if (!lineContent) {
+			return;
+		}
 		const LineEndCh = lineContent.length;
 		const lastLine = editor.lastLine();
 		console.log(LineEndCh, lastLine);
 		const lineContent1 = lineContent + "\n" + lineContent;
 		editor.setLine(lineNumber, lineContent1);
-		lineNumber++;
-		editor.setCursor(lineNumber, LineEndCh);
+		editor.setCursor(lineNumber + 1, LineEndCh);
 	}
 }
