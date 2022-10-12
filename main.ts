b88f6d25cd994d94746ce428cf4816003cf671da
1C0D <63018905+1C0D@users.noreply.github.com>
2022-10-13 01:31:37 +0200

    modification

--- main.ts
+++ main.ts
@@ -7,67 +7,20 @@ export default class WindowCollapse extends Plugin {
 			name: "Duplicate Line",
 			editorCallback: (editor) => this.duplicateLine(editor),
 		});
-		this.addCommand({
-			id: "arrow-down",
-			name: "Arrow Down",
-			hotkeys: [{ modifiers: ["Mod", "Alt"], key: "ArrowDown" }],
-			editorCallback: (editor) => {
-				this.arrows(editor, "↓");
-			},
-		});
-		this.addCommand({
-			id: "arrow-up",
-			name: "Arrow Up",
-			hotkeys: [{ modifiers: ["Mod", "Alt"], key: "ArrowUp" }],
-			editorCallback: (editor) => {
-				this.arrows(editor, "↑");
-			},
-		});
-		this.addCommand({
-			id: "arrow-left",
-			name: "Arrow Left",
-			hotkeys: [{ modifiers: ["Mod", "Alt"], key: "ArrowLeft" }],
-			editorCallback: (editor) => {
-				this.arrows(editor, "←");
-			},
-		});
-		this.addCommand({
-			id: "arrow-right",
-			name: "Arrow Right",
-			hotkeys: [{ modifiers: ["Mod", "Alt"], key: "ArrowRight" }],
-			editorCallback: (editor) => {
-				this.arrows(editor, "→");
-			},
-		});
-	}
-
-	arrows(editor, arrow) {
-		const { line: lineNumber, ch: column } = editor.getCursor();
-		const lineContent = editor.getLine(lineNumber);
-		const content0 = lineContent.slice(0, column);
-		const content1 = lineContent.slice(column, lineContent.length);
-		const newLineContent = content0 + arrow + content1;
-		editor.setLine(lineNumber, newLineContent);
-		editor.setCursor(lineNumber + 1, column + 1);
 	}
 
 	duplicateLine(editor) {
-		// const markdownView =
-		// 	this.app.workspace.getActiveViewOfType(MarkdownView);
-
-		// if (!markdownView) return;
-
-		// const editor = markdownView.editor;
 		const { line: lineNumber, ch: column } = editor.getCursor();
 		const lineContent = editor.getLine(lineNumber);
 		if (!lineContent) {
 			return;
 		}
-		const LineEndCh = lineContent.length;
-		const lastLine = editor.lastLine();
-		console.log(LineEndCh, lastLine);
 		const lineContent1 = lineContent + "\n" + lineContent;
-		editor.setLine(lineNumber, lineContent1);
-		editor.setCursor(lineNumber + 1, LineEndCh);
+		editor.replaceRange(
+			lineContent1,
+			{ line: lineNumber, ch: 0 },
+			{ line: lineNumber, ch: lineContent.length }
+		);
+		editor.setCursor(lineNumber + 1, lineContent.length);
 	}
 }
