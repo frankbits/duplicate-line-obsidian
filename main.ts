b6c2403289b16675905eb83f933f35dea4764e06
1C0D <63018905+1C0D@users.noreply.github.com>
2022-11-07 01:56:54 +0100

    multicursor and multiline selection

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { Editor, Plugin } from "obsidian";
+import { Editor, EditorSelectionOrCaret, Plugin } from "obsidian";
 
 export default class WindowCollapse extends Plugin {
 	async onload() {
@@ -9,17 +9,60 @@ export default class WindowCollapse extends Plugin {
 		});
 	}
 	duplicateLine(editor: Editor) {
-		const { line: lineNumber } = editor.getCursor();
-		const lineContent = editor.getLine(lineNumber);
-		if (!lineContent) {
-			return;
-		}
-		const lineContent1 = lineContent + "\n" + lineContent;
-		editor.replaceRange(
-			lineContent1,
-			{ line: lineNumber, ch: 0 },
-			{ line: lineNumber, ch: lineContent.length }
-		);
-		editor.setCursor(lineNumber + 1, lineContent.length);
+		const cursors = editor.listSelections(); //multicursors
+		let addedLines = 0;
+		const selections: EditorSelectionOrCaret[] = [];
+		cursors.forEach((cursor) => {
+			let lineContent = "";
+			let lineNumber = 0;
+			const head = cursor.head.line;
+			const anchor = cursor.anchor.line;
+			const headChar = cursor.head.ch;
+			const anchorChar = cursor.anchor.ch;
+			if (head == anchor) {
+				lineNumber = head + addedLines;
+				lineContent = editor.getLine(lineNumber);
+				if (!lineContent) {
+					return;
+				}
+				addedLines++;
+				const lineContent1 = lineContent + "\n" + lineContent;
+				editor.replaceRange(
+					lineContent1,
+					{ line: lineNumber, ch: 0 },
+					{ line: lineNumber, ch: lineContent.length }
+				);
+			} else {
+				let totalContent = "";
+				const lastLine = Math.max(head, anchor) + addedLines;
+				const firstLine = Math.min(head, anchor) + addedLines;
+				const linesRange = Math.abs(head - anchor);
+
+				for (let i = 0; i <= linesRange; i++) {
+					const activeLine = firstLine + i;
+					lineContent = editor.getLine(activeLine);
+					if (i !== linesRange) totalContent += lineContent + "\n";
+					else totalContent += lineContent;
+				}
+				addedLines += linesRange + 1;
+
+				const lastLineContent = editor.getLine(lastLine);
+
+				const lineContent1 = lastLineContent + "\n" + totalContent;
+				editor.replaceRange(
+					lineContent1,
+					{ line: lastLine, ch: 0 },
+					{ line: lastLine, ch: lastLineContent.length }
+				);
+			}
+			//to set selection back
+			selections.push({
+				anchor: { line: anchor + addedLines, ch: anchorChar },
+				head: { line: head + addedLines, ch: headChar },
+			});
+		});
+		editor.setSelection({ line: 0, ch: 0 }, { line: 0, ch: 0 });//deselect all
+		editor.setSelections(selections);
 	}
 }
+
