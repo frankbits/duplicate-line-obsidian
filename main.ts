5b9071389875748878a38a51f5a9846551589f7e
1C0D <63018905+1C0D@users.noreply.github.com>
2023-08-28 02:41:04 +0200

    added dupli select up & down

--- main.ts
+++ main.ts
@@ -14,6 +14,8 @@ import { sortBy } from "lodash";
 enum Direction {
 	Up,
 	Down,
+	SelDown,
+	SelUp,
 	Left,
 	Right,
 }
@@ -45,10 +47,16 @@ export default class DuplicateLine extends Plugin {
 				this.duplicateLine(editor, Direction.Up),
 		});
 		this.addCommand({
-			id: "duplicate-line-left",
-			name: "Duplicate Line Left",
+			id: "duplicate-selection-down",
+			name: "Duplicate Selection Down",
 			editorCallback: (editor) =>
-				this.duplicateLine(editor, Direction.Left),
+				this.duplicateLine(editor, Direction.SelDown),
+		});
+		this.addCommand({
+			id: "duplicate-selection-up",
+			name: "Duplicate Selection Up",
+			editorCallback: (editor) =>
+				this.duplicateLine(editor, Direction.SelUp),
 		});
 		this.addCommand({
 			id: "duplicate-line-right",
@@ -56,6 +64,13 @@ export default class DuplicateLine extends Plugin {
 			editorCallback: (editor) =>
 				this.duplicateLine(editor, Direction.Right),
 		});
+		// useless
+		// this.addCommand({ 
+		// 	id: "duplicate-line-left",
+		// 	name: "Duplicate Line Left",
+		// 	editorCallback: (editor) =>
+		// 		this.duplicateLine(editor, Direction.Left),
+		// });
 	}
 
 	async loadSettings() {
@@ -81,7 +96,9 @@ export default class DuplicateLine extends Plugin {
 				selection,
 				direction
 			);
-			const rangeLine = this.selectionToRange(newSelection); //already sorted
+			console.debug("newSelection", newSelection)
+			const rangeLine = this.selectionToRange(newSelection, true); //already sorted
+			console.debug("rangeLine", rangeLine)
 			const numberOfLines = rangeLine.to.line - rangeLine.from.line + 1;
 			let content = editor.getRange(rangeLine.from, rangeLine.to);
 			if (!content.trim()) continue;
@@ -95,7 +112,7 @@ export default class DuplicateLine extends Plugin {
 				line: 0,
 				ch: 0,
 			};
-			
+
 			switch (direction) {
 				case Direction.Down:
 					addedLines += numberOfLines;
@@ -138,7 +155,7 @@ export default class DuplicateLine extends Plugin {
 					break;
 
 				case Direction.Left: {
-					if (this.settings.addSpaceBetween) content = content + " "  
+					if (this.settings.addSpaceBetween) content = content + " "
 					newAnchor = {
 						line: selection.anchor.line,
 						ch: selection.anchor.ch,
@@ -156,10 +173,10 @@ export default class DuplicateLine extends Plugin {
 				}
 
 				case Direction.Right: {
-					if (this.settings.addSpaceBetween) content = " " + content 
+					if (this.settings.addSpaceBetween) content = " " + content
 					newAnchor = {
 						line: selection.anchor.line,
-						ch: selection.anchor.ch+content.length,
+						ch: selection.anchor.ch + content.length,
 					};
 					newHead = {
 						line: selection.head.line,
@@ -170,6 +187,64 @@ export default class DuplicateLine extends Plugin {
 						to: rangeLine.to,
 						text: content,
 					};
+					break;
+				}
+				case Direction.SelDown: {
+					addedLines += numberOfLines;
+					const toLength = editor.getLine(rangeLine.to.line).length
+					const NewrangeLineTo = { line: rangeLine.to.line, ch: toLength }
+					const condition = this.areObjectsEqual(selection.head, rangeLine.to)
+
+					newAnchor = {
+						line: selection.anchor.line + addedLines,
+						ch: condition ?
+							0 : Math.abs(selection.anchor.ch - selection.head.ch)
+					};
+
+					newHead = {
+						line: selection.head.line + addedLines,
+						ch: condition ?
+							Math.abs(selection.anchor.ch - selection.head.ch) : 0,
+					};
+
+					{
+						change = {
+							from: NewrangeLineTo,
+							to: NewrangeLineTo,
+							text: "\n" + content,
+						};
+					}
+
+					break;
+				}
+				case Direction.SelUp: {
+					addedLines += numberOfLines;
+					const condition = this.areObjectsEqual(selection.head, rangeLine.to)
+					const NewrangeLineFrom = { line: rangeLine.from.line, ch: 0 }
+					newAnchor = {
+						line: selection.anchor.line + addedLines-1,
+						ch: condition ? 0 : Math.abs(selection.anchor.ch - selection.head.ch),
+					};
+					newHead = {
+						line: selection.head.line + addedLines-1,
+						ch: condition ? Math.abs(selection.anchor.ch - selection.head.ch) : 0,
+					};
+					// newAnchor = {
+					// 	line: selection.anchor.line + addedLines,
+					// 	ch: selection.anchor.ch,
+					// };
+					// newHead = {
+					// 	line: selection.head.line + addedLines,
+					// 	ch: selection.head.ch,
+					// };
+
+					{
+						change = {
+							from: NewrangeLineFrom,
+							to: NewrangeLineFrom,
+							text: content + "\n",
+						};
+					}
 					break;
 				}
 			}
@@ -194,6 +269,10 @@ export default class DuplicateLine extends Plugin {
 			editor.transaction(transaction, origin);
 		}
 	};
+
+	areObjectsEqual(obj1: any, obj2: any) {
+		return obj1.line === obj2.line && obj1.ch === obj2.ch;
+	}
 
 	selectionToRange(selection: EditorSelection, sort?: boolean): EditorRange {
 		const positions = [selection.anchor, selection.head];
@@ -222,7 +301,10 @@ export default class DuplicateLine extends Plugin {
 				head: { line: range.to.line, ch: toLength },
 			};
 			return newSelection;
-		} else {
+		} else if (direction === Direction.SelDown) {
+			return selection;
+		}
+		else {
 			// no selection (testing word before cursor)
 			if (range.from.line === range.to.line && range.from.ch === range.to.ch) {
 				const line = range.from.line
@@ -237,14 +319,14 @@ export default class DuplicateLine extends Plugin {
 					startOfWord++;
 					// extract previous word
 					const contentLength = currentLine.slice(startOfWord, ch).length;
-					const newSelection: EditorSelection =  {
+					const newSelection: EditorSelection = {
 						anchor: { line: line, ch: (ch - contentLength) },
 						head: { line: line, ch: (ch) },
 					};
 					return newSelection
 				}
-				
-			} 
+
+			}
 			return {
 				anchor: editor.getCursor("from"),
 				head: editor.getCursor("to"),
